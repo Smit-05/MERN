e0caf4dbcc4cff9d135f8e76e2236ede8b58e07d
Smit Padaliya <smitpadaliya5@gmail.com>
2022-12-02 01:04:23 +0530

    Update Scroll.js
    Co-Authored-By: Vashishth Patel <76911582+vasu-1@users.noreply.github.com>

--- client/src/components/Scroll.js
+++ client/src/components/Scroll.js
@@ -41,39 +41,6 @@ function Scroll(){
     </div>
   )
 }
-// class Scroll extends React.Component {
-//   state = {
-//     items: Array.from({ length: 20 })
-//   };
 
-//   fetchMoreData = () => {
-//     // a fake async api call like which sends
-//     // 20 more records in 1.5 secs
-//     setTimeout(() => {
-//       this.setState({
-//         items: this.state.items.concat(Array.from({ length: 20 }))
-//       });
-//     }, 1500);
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <InfiniteScroll
-//           dataLength={this.state.items.length}
-//           next={this.fetchMoreData}
-//           hasMore={true}
-//           loader={<h4>Loading...</h4>}
-//         >
-//           {this.state.items.map((i, index) => (
-//             <div style={style} key={index}>
-//               div - #{index}
-//             </div>
-//           ))}
-//         </InfiniteScroll>
-//       </div>
-//     );
-//   }
-// }
 
 export default Scroll;
